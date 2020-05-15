4b82328fe660c2f5d9d6d56b98c222d7cbb6e0af
simoncar <simoncar@users.noreply.github.com>
2020-05-15 10:11:33 +0000

    deploy: ce4d384c2c790585a29a1b5e11df4f75af3f14d0

---
+++ precache-manifest.c5659bf287d11547e4c2267fb352b99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "271c6363b66f9db33b32",
+    "url": "/static/css/main.16dd7bee.chunk.css"
+  },
+  {
+    "revision": "271c6363b66f9db33b32",
+    "url": "/static/js/main.271c6363.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "409fb0a27f8c96ac974d",
+    "url": "/static/css/2.ccc5485d.chunk.css"
+  },
+  {
+    "revision": "409fb0a27f8c96ac974d",
+    "url": "/static/js/2.409fb0a2.chunk.js"
+  },
+  {
+    "revision": "d131d1c1044311b6bb74d69276ddaab3",
+    "url": "/index.html"
+  }
+];
